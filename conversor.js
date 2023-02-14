539a77a808714cd8d48c6620dee11b892fdb9a5c
Adrianシロ <adrianstratos@gmail.com>
2023-02-14 11:32:36 -0500

    conversiones faltantes

--- conversor.js
+++ conversor.js
@@ -16,14 +16,13 @@ function regresar_num(num) {
         return String.fromCharCode(num + 55)
     }
 }
+
 function a_decimal(base, str_num){
     let ult_pos = str_num.length
     let num = 0
     let exp = 0
-    // prueba
     while (str_num != "" && exp < 12) {
         str_act = str_num.slice(--ult_pos)
-        console.log("bit actual:", str_act)
         if(str_act.match(/[A-F]/)){
             num += (str_act.charCodeAt(0) - 55)*base**exp
         } else {
@@ -32,14 +31,13 @@ function a_decimal(base, str_num){
         
         exp++
         str_num = str_num.substring(0, ult_pos)
-        console.log("Nuevo string", str_num)
     }
     return num
 }
+
 function binario_a_otra_base(str_num, base_a_convertir) {
     var tam = str_num.length
     var ret = ""
-    console.log(str_num, tam, base_a_convertir)
     let nbits, ult_pos
     if (base_a_convertir == 10) {
         return a_decimal(2,str_num)
@@ -49,16 +47,10 @@ function binario_a_otra_base(str_num, base_a_convertir) {
         nbits = 4
     }
     ult_pos = tam - nbits
-    // prueba
     while (str_num.length > 0) {
         str_act = str_num.slice(ult_pos)
-        //console.log("bit actual:", str_act)
-        
-        
         str_num = str_num.substring(0, ult_pos)
-        //console.log("Nuevo string", str_num) 
         ret = regresar_num(a_decimal(2,str_act)) + ret
-        console.log(ret)
         if(ult_pos > nbits)
             ult_pos -= nbits
         else 
@@ -68,15 +60,20 @@ function binario_a_otra_base(str_num, base_a_convertir) {
 }
 
 function octal_a_otra_base(str_num, base_a_convertir) {
-    if(base_a_convertir == 10)
+    if(base_a_convertir == 10){
         return a_decimal(8, str_num)
-    return "Por hacer jaja"
+    }else if(base_a_convertir == 16){
+        var strDecimal = a_decimal(8, str_num)
+        return (strDecimal.toString(16)).toUpperCase();
+    }else if(base_a_convertir == 2){
+        var strDecimal = a_decimal(8, str_num);
+        return (strDecimal.toString(2));
+    }
 }
 
 function decimal_a_otra_base(str_num, base_a_convertir) {
     var num = parseInt(str_num)
     var ret = ""
-    //console.log(num, base_a_convertir)
     while (num >= base_a_convertir) {
         let mod = num % base_a_convertir
         ret = regresar_num(mod) + ret
@@ -86,7 +83,13 @@ function decimal_a_otra_base(str_num, base_a_convertir) {
 }
 
 function hexadecimal_a_otra_base(str_num, base_a_convertir) {
-    if(base_a_convertir == 10)
+    if(base_a_convertir == 10){
         return a_decimal(16, str_num)
-    return "Por hacer jaja"
+    }else if(base_a_convertir == 2){
+        var strDecimal = a_decimal(16, str_num)
+        return (strDecimal.toString(2));
+    }else if(base_a_convertir == 8){
+        var strDecimal = a_decimal(16, str_num)
+        return (strDecimal.toString(8));
+    }
 }
